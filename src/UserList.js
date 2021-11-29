885747c4eef57a2e7b9f2e921a69d65351dd97f7
rninsung <kms73033222@gmail.com>
2021-11-29 22:02:24 +0900

    react
    useCallback, useMemo

--- src/UserList.js
+++ src/UserList.js
@@ -1,10 +1,9 @@
 import React, { useEffect } from "react";
 
 function User({ user, onRemove, onToggle }) {
-  const { username, email, id, active } = user;
   useEffect(() => {
     console.log(user);
-  }, [user]);
+  });
   return (
     <div>
       <b
@@ -16,6 +15,7 @@ function User({ user, onRemove, onToggle }) {
       >
         {user.username}
       </b>
+      &nbsp;
       <span>({user.email})</span>
       <button onClick={() => onRemove(user.id)}>삭제</button>
     </div>
